eda932b15db8108de23380a32485f2381a41dc40
Frechenstein <j.f2514@gmx.de>
2023-12-06 22:54:02 +0100

    lasers and movement

--- Asteroid Shooter/asteroid_shooter.py
+++ Asteroid Shooter/asteroid_shooter.py
@@ -1,24 +1,37 @@
 import pygame
 import sys
 
-pygame.init()
+def laser_update(laser_list, speed = 300):
+    for rect in laser_list:
+        rect.y -= round(speed * dt)
+        if rect.bottom < 0:
+            laser_list.remove(rect)
 
 # constants
 SCREEN_WIDTH, SCREEN_HEIGHT = 1280, 720
+FRAMERATE = 120
 PATH = 'pygame-testing/Asteroid Shooter'
 FONT_SIZE = 50
 
-# initialize screen
+# game init
+pygame.init()
 display_surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Asteriod Shooter')
 clock = pygame.time.Clock()
 
-# importing images
+# ship import
 ship_surf = pygame.image.load(PATH + '/graphics/ship.png').convert_alpha()
-ship_rect = ship_surf.get_rect(center = (SCREEN_WIDTH / 2, SCREEN_HEIGHT))
+ship_rect = ship_surf.get_rect(center = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2))
+
+# laser import
+laser_surf = pygame.image.load(PATH + '/graphics/laser.png').convert_alpha()
+laser_list = []
+# laser_rect = laser_surf.get_rect(midbottom = ship_rect.midtop)
+
+# background
 bg_surf = pygame.image.load(PATH + '/graphics/background.png').convert()
 
-# import text
+# text import
 font = pygame.font.Font(PATH + '/graphics/subatomic.ttf', FONT_SIZE)
 text_surf = font.render('Space', True, 'white')
 text_rect = text_surf.get_rect(center = (SCREEN_WIDTH / 2, SCREEN_HEIGHT - 80))
@@ -26,22 +39,37 @@ text_rect = text_surf.get_rect(center = (SCREEN_WIDTH / 2, SCREEN_HEIGHT - 80))
 # main game loop
 while True:
     
+    # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
             
-    # framerate limit
-    clock.tick(120)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+             print('laser')
+             laser_rect = laser_surf.get_rect(midbottom = ship_rect.midbottom)
+             laser_list.append(laser_rect)
             
-    # mouse input
+    # framerate limit
+    dt = clock.tick(FRAMERATE) / 1000	
+
+	# mouse input 
     ship_rect.center = pygame.mouse.get_pos()
+
+	# update
+    laser_update(laser_list)
     
-    # updates
+    # drawing
     display_surface.fill((0, 0, 0))
     display_surface.blit(bg_surf,(0, 0))
     
-    display_surface.blit(ship_surf,ship_rect)
     display_surface.blit(text_surf,text_rect)
+    pygame.draw.rect(display_surface, 'white', text_rect.inflate(30, 30), width = 8, border_radius = 5)
     
+    for rect in laser_list:
+        display_surface.blit(laser_surf,rect)
+        
+    display_surface.blit(ship_surf,ship_rect)
+    
+    # draw the final frame
     pygame.display.update()
