5d733f799077bd1d37a6db82c71cf8b3009073c4
Frechenstein <j.f2514@gmx.de>
2023-12-06 18:35:38 +0100

    graphics import and image.blit

--- Asteroid Shooter/asteroid_shooter.py
+++ Asteroid Shooter/asteroid_shooter.py
@@ -5,9 +5,21 @@ pygame.init()
 
 # constants
 SCREEN_WIDTH, SCREEN_HEIGHT = 1280, 720
+PATH = 'pygame-testing/Asteroid Shooter'
+FONT_SIZE = 50
 
+# initialize screen
 display_surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Asteriod Shooter")
+pygame.display.set_caption('Asteriod Shooter')
+
+# importing images
+ship_surf = pygame.image.load(PATH + '/graphics/ship.png').convert_alpha()
+bg_surf = pygame.image.load(PATH + '/graphics/background.png').convert()
+
+# import text
+font = pygame.font.Font(PATH + '/graphics/subatomic.ttf', FONT_SIZE)
+text_surf = font.render('Space', True, 'white')
+
 
 while True:
     
@@ -18,5 +30,8 @@ while True:
             
     # updates
     display_surface.fill((0, 0, 0))
+    display_surface.blit(bg_surf,(0, 0))
+    display_surface.blit(ship_surf,(300, 500))
+    display_surface.blit(text_surf,(SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
     
     pygame.display.update()
